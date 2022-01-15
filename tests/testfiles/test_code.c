327916fdc336598cb54152b5f2f1bfc5501df595
Bartosz Szpila <bartoszszpila98@gmail.com>
2022-01-15 13:47:23 +0100

    Added more types to test_code

--- tests/testfiles/test_code.c
+++ tests/testfiles/test_code.c
@@ -7,9 +7,24 @@ typedef struct TestStruct_tag
     void *c;
 } TestStruct_t;
 
-enum Values {
+struct BitFields
+{
+    int a : 1;
+    int b : 2;
+    int c : 3;
+    int d : 2;
+};
+
+enum Values
+{
     VAL_FIRST = 0,
     VAL_SECOND = 3
+};
+
+union Unity
+{
+    char (*function_ptr)(int, char);
+    int c;
 };
 
 TestStruct_t structs[3] = {
@@ -23,10 +38,20 @@ char test_function(size_t elem)
     return structs[elem].b;
 }
 
+static char static_function(int count, char character)
+{
+    return character + count;
+}
+
+char (*my_pointer)(int, char) = static_function;
+
 int main(int argc, char *argv[])
 {
+    union Unity a = {.function_ptr = my_pointer};
+
     printf("%c\n", test_function(1));
     printf("%d\n", VAL_FIRST);
+    printf("%d\n", a.function_ptr(3, 'a'));
 
     return 0;
 }
